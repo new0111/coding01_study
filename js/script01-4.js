a5736637ea9a25c9fa588d39c67e31e64afb843f
ccy <cocodyd4591@gmail.com>
2022-01-07 18:20:29 +0900

    220107

---
+++ js/script01-4.js
@@ -0,0 +1,55 @@
+const amIFat = true;
+let something;
+console.log(something, amIFat);
+
+const daysOfWeek = ["mon" , "tue", "wed", "thu", "fri", "sat"];
+// Get Item from Array
+console.log(daysOfWeek);
+
+//Add one more day to the array
+daysOfWeek.push("sun");
+
+console.log(daysOfWeek);
+
+const toBuy = ["potato", "tomato", "pizza"];
+toBuy.push("apple");
+
+/* alert("hi"); */
+
+/* const a = 5;
+const b = 2;
+let myName = "Bab";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("Hello" + myName);
+
+myName = "juice";
+console.log("your" + myName);  */
+
+/* 변수만들때 let, const, var차이
+let 재선언 금지, 재할당 가능
+const 재선언 금지, 재할당 금지
+var 재선언 가능, 재할당 가능
+
+let a = b;
+let a = c;
+//재선언 금지
+
+let a = b;
+a = c;
+//재할당은 가능
+
+const a = b;
+const a = c;
+//재선언 금지
+
+const a = b;
+a = c;
+//재할당 금지
+
+var a = b;
+var a = c;
+a = d;
+//재선언, 재할당 가능 */
