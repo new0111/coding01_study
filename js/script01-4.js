5a66346f50e895638d29ef1a5ded5f95038a4563
ccy <cocodyd4591@gmail.com>
2022-01-08 19:24:15 +0900

    220108

--- js/script01-4.js
+++ js/script01-4.js
@@ -1,4 +1,117 @@
-const amIFat = true;
+const age = 96;
+function calculateKrAge(ageOfForeigner) {
+    return ageOfForeigner + 2;
+};
+
+const KrAge = calculateKrAge(age);
+
+console.log(KrAge);
+
+const calculator = {
+    add : function(a,b) {
+        return a + b;
+    },
+
+    min : function(a, b) {
+        return a - b;
+    },
+
+    mul : function(a, b) {
+        return a * b;
+    },
+
+    div : function(a,b) {
+        return a / b;
+    },
+
+    pow: function(a, b) {
+        return a ** b;
+    },
+};
+
+const plusResult = calculator.add(2,4);
+const minusResult =calculator.min(plusResult,4);
+const mulResult =calculator.mul(20,4);
+const divideResult =calculator.div(60,4);
+const powerResult =calculator.pow(4,4);
+
+
+
+/* const calculator = {
+    add : function(a,b) {
+        console.log(a + b);
+    },
+
+    min : function(a, b) {
+        console.log(a - b);
+    },
+
+    mul : function(a, b) {
+        console.log(a * b);
+    },
+
+    div : function(a,b) {
+        console.log(a / b);
+    },
+
+    pow: function(a, b) {
+        console.log(a ** b);
+    },
+};
+
+calculator.add(2,4);
+calculator.min(5,4);
+calculator.mul(20,4);
+calculator.div(60,4);
+calculator.pow(4,4); */
+
+/* const player = {
+    name : "Bob",
+    sayHello : function (otherPersonsName) {
+        console.log("hello " + otherPersonsName + " nice to meet you");
+    },
+};
+
+console.log(player.name);
+player.sayHello("lynn");
+console.log(console);
+ */
+
+
+/* function plus(a, b) {
+    console.log(a + b);
+};
+
+function divide() {
+    console.log(a / b);
+};
+
+plus(8,20);
+divide(90, 20);
+
+function sayHello(nameOfPerson, age) {
+    console.log("Hello my name is " + nameOfPerson + " and I'm " + age);
+}
+
+sayHello("nico", 12);
+sayHello("nice", 32);
+sayHello("Bob", 35); */
+
+
+/* const player = {
+    name:"Bob",
+    points: 10,
+    fat: false,
+};
+
+console.log(player);
+console.log(player["name"]);
+player.lastName ="potato";
+player.points += 15;
+console.log(player["points"]); */
+
+
+/* const amIFat = true;
 let something;
 console.log(something, amIFat);
 
@@ -12,7 +125,7 @@ daysOfWeek.push("sun");
 console.log(daysOfWeek);
 
 const toBuy = ["potato", "tomato", "pizza"];
-toBuy.push("apple");
+toBuy.push("apple"); */
 
 /* alert("hi"); */
 
