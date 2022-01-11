5d51fb242e986a75598191eb6f01522d673de7da
ccy <cocodyd4591@gmail.com>
2022-01-11 18:03:06 +0900

    220111

--- js/script01-4.js
+++ js/script01-4.js
@@ -1,15 +1,65 @@
-const title = document.querySelector("div.hello:first-child h1");
+const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-    console.log("title was clicked!");
-    title.style.color = "blue";
+    //const clickedClass = "clicked"
+   h1.classList.toggle("clicked");
+   
+   
+    // if (h1.classList.contains(clickedClass)) {
+    //     h1.classList.remove(clickedClass);
+    // }
+    // else {
+    //     h1.classList.add(clickedClass);
+    // }
+
+
+    // const currentColor = h1.style.color;
+    // let newColor;
+    // if(currentColor === "blue") {
+    //     newColor = "tomato";
+    // }
+    // else {
+    //     newColor = "blue";
+    // }
+    // h1.style.color = newColor;
 }
 
-console.dir(title);
+// function handleMouseEnter() {
+//     h1.innerText = "mouse is here!";
+// }
+
+// function handleMouseLeave() {
+//     h1.innerText = "Mouse is gone!";
+// }
+
+// function handleWindowResize() {
+//     document.body.style.backgroundColor = "tomato";
+// }
+
+// function handleWindowCopy() {
+//     alert("copier!");
+// }
+
+// function handleWindowOffline() {
+//     alert("SOS no WIFI!!");
+// }
+
+// function handleWindowOnline() {
+//     alert("ALL GOOOOD!");
+// }
+
+console.dir(h1);
 
 
-title.addEventListener("click", handleTitleClick);
+h1.addEventListener("click", handleTitleClick);
+// h1.onclick = handleTitleClick;
+// h1.addEventListener("mouseenter", handleMouseEnter);
+// h1.addEventListener("mouseleave", handleMouseLeave);
 
+// window.addEventListener("resize", handleWindowResize);
+// window.addEventListener("copy", handleWindowCopy);
+// window.addEventListener("offline", handleWindowOffline);
+// window.addEventListener("online", handleWindowOnline);
 
 /* const title = document.getElementById("title");
 title.innerText = "got you!";
---
+++ js/script_test05.js
@@ -0,0 +1,11 @@
+//const loginForm = document.querySelector("#login-form");
+// const loginInput = loginForm.querySelector("input");
+const loginInput = document.querySelector("#login-form input");
+// const loginButton = loginForm.querySelector("button");
+const loginButton = document.querySelector("#login-form button");
+
+function handleLoginBtnClick() {
+
+}
+
+loginButton.addEventListener("click", handleTitleClick);
