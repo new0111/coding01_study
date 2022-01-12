8440da21408d48b1cbd37b357656f52980a179e9
ccy <cocodyd4591@gmail.com>
2022-01-12 18:03:57 +0900

    220112

--- js/script_test05.js
+++ js/script_test05.js
@@ -1,11 +1,32 @@
 //const loginForm = document.querySelector("#login-form");
 // const loginInput = loginForm.querySelector("input");
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 // const loginButton = loginForm.querySelector("button");
-const loginButton = document.querySelector("#login-form button");
+// const loginButton = document.querySelector("#login-form button");
+const link = document.querySelector("a");
 
-function handleLoginBtnClick() {
 
+function onLoginSubmit(event) {
+    event.preventDefault();
+    console.log(event);
 }
 
-loginButton.addEventListener("click", handleTitleClick);
+function handleLinkClick() {
+    alert("clicked");
+}
+
+
+// function handleLoginBtnClick() {
+//     const username =loginInput.value;
+//     if(username === "") {
+//         alert("Please write your name");
+//     }
+//     else if (username.length > 15) {
+//         alert("your name is too long.");
+//     }
+// }
+
+// loginButton.addEventListener("click", handleLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
