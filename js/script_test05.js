e80b93ee8f6d169ec7fa211c308115cefc916fd1
ccy <cocodyd4591@gmail.com>
2022-01-13 18:29:12 +0900

    220113

--- js/script_test05.js
+++ js/script_test05.js
@@ -4,17 +4,31 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 // const loginButton = loginForm.querySelector("button");
 // const loginButton = document.querySelector("#login-form button");
-const link = document.querySelector("a");
+// const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting")
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 
 function onLoginSubmit(event) {
     event.preventDefault();
-    console.log(event);
+    // console.log(event);
+    // const typeUsername = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    localStorage.setItem(USERNAME_KEY, loginInput.value);
+    // greeting.innerText = "Hello " + username;
+    paintGreeting();
 }
 
-function handleLinkClick() {
-    alert("clicked");
+function paintGreeting() {
+    const username = localStorage.getItem(USERNAME_KEY);
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${username}`;
 }
+
+// function handleLinkClick() {
+//     alert("clicked");
+// }
 
 
 // function handleLoginBtnClick() {
@@ -28,5 +42,17 @@ function handleLinkClick() {
 // }
 
 // loginButton.addEventListener("click", handleLoginBtnClick);
-loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
+
+// link.addEventListener("click", handleLinkClick);
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null) {
+    // show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}
+else {
+    // show the greetings
+    paintGreeting();
+}
