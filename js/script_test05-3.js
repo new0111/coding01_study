0e8215b8c0e41d8d330842a94f3c7ef6ed40ad27
ccy <cocodyd4591@gmail.com>
2022-01-14 14:39:35 +0900

    220114

---
+++ js/script_test05-3.js
@@ -0,0 +1,50 @@
+const quotes = [
+    {
+        quote: "When you go through hardships and decide not to surrender, that is strength.",
+        author: "Arnold Schwarzenegger",
+    },
+    {
+        quote: "It is kind of fun to do the impossible.",
+        author: "Walt Disney",
+    },
+    {
+        quote: "Tough times never last, but tough people do.",
+        author: "Robert H. Schuller",
+    },
+    {
+        quote: "I’ve failed over and over and over again in my life and that is why I succeed.",
+        author: "Michael Jordan",
+    },
+    {
+        quote: "If you don’t get out of the box you’ve been raised in, you won’t understand how much bigger the world is.",
+        author: "Angelina Jolie",
+    },
+    {
+        quote: "I didn’t get there by wishing for it or hoping for it, but by working for it.",
+        author: "Estée Lauder",
+    },
+    {
+        quote: "It is better to fail in originality than to succeed in imitation.",
+        author: "Herman Melville",
+    },
+    {
+        quote: "Success is walking from failure to failure with no loss of enthusiasm.",
+        author: "Winston Churchill",
+    },
+    {
+        quote: "Success seems to be connected with action. Successful people keep moving.",
+        author: "Conrad Hilton",
+    },
+    {
+        quote: "In order to succeed, we must first believe that we can.",
+        author: "Nikos Kazantzakis",
+    },
+];
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+ 
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.author;
